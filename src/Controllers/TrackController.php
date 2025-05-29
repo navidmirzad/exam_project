8058d15a79be13784d6cfa11eb74aeb16e451fc5
navidmirzad <navi0221@stud.kea.dk>
2025-05-29 12:35:21 +0200

    json

--- src/Controllers/TrackController.php
+++ src/Controllers/TrackController.php
@@ -20,10 +20,8 @@ class TrackController
             try {
                 $search = trim($_GET['s']);
                 $tracks = $this->trackModel->search($search);
-                if (empty($tracks)) {
-                    $this->sendError('No tracks found', 404);
-                    return;
-                }
+                http_response_code(200);
+                echo json_encode($tracks);
             } catch (\Exception $e) {
                 $this->sendError($e->getMessage(), 500);
                 return;
